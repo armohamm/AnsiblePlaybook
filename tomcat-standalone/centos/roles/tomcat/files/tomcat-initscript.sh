c3fdccf2fe851cba897b4b0dd13ffd0bc48404ec
Anuj Sharma <anuj_sharma401@yahoo.com>
2017-12-13 08:00:04 +0530

    Update tomcat-initscript.sh

--- tomcat-standalone/centos/roles/tomcat/files/tomcat-initscript.sh
+++ tomcat-standalone/centos/roles/tomcat/files/tomcat-initscript.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
 #
-# chkconfig: 345 99 28
-# description: Starts/Stops Apache Tomcat
+# description: Apache Tomcat init script
+# processname: tomcat  
+# chkconfig: 234 20 80  
 #
-# Tomcat 7 start/stop/status script
-# Forked from: https://gist.github.com/valotas/1000094
-# @author: Miglen Evlogiev <bash@miglen.com>
 #
-# Release updates:
-# Updated method for gathering pid of the current proccess
-# Added usage of CATALINA_BASE
-# Added coloring and additional status
-# Added check for existence of the tomcat user
+# Copyright (C) 2014 Miglen Evlogiev
 #
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Initially forked from: gist.github.com/valotas/1000094
+# Source: gist.github.com/miglen/5590986
+
  
 #Location of JAVA_HOME (bin files)
 export JAVA_HOME=/usr/lib/jvm/jre
- 
+
 #Add Java binary files to PATH
 export PATH=$JAVA_HOME/bin:$PATH
  
@@ -24,13 +33,13 @@ export PATH=$JAVA_HOME/bin:$PATH
 export CATALINA_HOME=/usr/share/tomcat  
  
 #CATALINA_BASE is the location of the configuration files of this instance of Tomcat
-export CATALINA_BASE=/usr/share/tomcat
+export CATALINA_BASE=/usr/share/tomcat 
  
 #TOMCAT_USER is the default user of tomcat
 export TOMCAT_USER=tomcat
  
 #TOMCAT_USAGE is the message if this script is called without any options
-TOMCAT_USAGE="Usage: $0 {\e[00;32mstart\e[00m|\e[00;31mstop\e[00m|\e[00;32mstatus\e[00m|\e[00;31mrestart\e[00m}"
+TOMCAT_USAGE="Usage: $0 {\e[00;32mstart\e[00m|\e[00;31mstop\e[00m|\e[00;31mkill\e[00m|\e[00;32mstatus\e[00m|\e[00;31mrestart\e[00m}"
  
 #SHUTDOWN_WAIT is wait time in seconds for java proccess to stop
 SHUTDOWN_WAIT=20
@@ -49,11 +58,12 @@ start() {
     echo -e "\e[00;32mStarting tomcat\e[00m"
     #ulimit -n 100000
     #umask 007
-    #/bin/su -p -s /bin/sh tomcat
+    #/bin/su -p -s /bin/sh $TOMCAT_USER
         if [ `user_exists $TOMCAT_USER` = "1" ]
         then
-                su $TOMCAT_USER -c $CATALINA_HOME/bin/startup.sh
+                /bin/su $TOMCAT_USER -c $CATALINA_HOME/bin/startup.sh
         else
+                echo -e "\e[00;31mTomcat user $TOMCAT_USER does not exists. Starting with $(id)\e[00m"
                 sh $CATALINA_HOME/bin/startup.sh
         fi
         status
@@ -63,17 +73,25 @@ start() {
  
 status(){
           pid=$(tomcat_pid)
-          if [ -n "$pid" ]; then echo -e "\e[00;32mTomcat is running with pid: $pid\e[00m"
-          else echo -e "\e[00;31mTomcat is not running\e[00m"
+          if [ -n "$pid" ]
+            then echo -e "\e[00;32mTomcat is running with pid: $pid\e[00m"
+          else
+            echo -e "\e[00;31mTomcat is not running\e[00m"
+            return 3
           fi
 }
- 
+
+terminate() {
+	echo -e "\e[00;31mTerminating Tomcat\e[00m"
+	kill -9 $(tomcat_pid)
+}
+
 stop() {
   pid=$(tomcat_pid)
   if [ -n "$pid" ]
   then
     echo -e "\e[00;31mStoping Tomcat\e[00m"
-    #/bin/su -p -s /bin/sh tomcat
+    #/bin/su -p -s /bin/sh $TOMCAT_USER
         sh $CATALINA_HOME/bin/shutdown.sh
  
     let kwait=$SHUTDOWN_WAIT
@@ -86,8 +104,8 @@ stop() {
     done
  
     if [ $count -gt $kwait ]; then
-      echo -n -e "\n\e[00;31mkilling processes which didn't stop after $SHUTDOWN_WAIT seconds\e[00m"
-      kill -9 $pid
+      echo -n -e "\n\e[00;31mkilling processes didn't stop after $SHUTDOWN_WAIT seconds\e[00m"
+      terminate
     fi
   else
     echo -e "\e[00;31mTomcat is not running\e[00m"
@@ -105,27 +123,25 @@ user_exists(){
 }
  
 case $1 in
- 
-        start)
-          start
-        ;;
-       
-        stop)  
-          stop
-        ;;
-       
-        restart)
-          stop
-          start
-        ;;
-       
-        status)
-                status
-               
-        ;;
-       
-        *)
-                echo -e $TOMCAT_USAGE
-        ;;
+	start)
+	  start
+	;;
+	stop)  
+	  stop
+	;;
+	restart)
+	  stop
+	  start
+	;;
+	status)
+		status
+		exit $?  
+	;;
+	kill)
+		terminate
+	;;		
+	*)
+		echo -e $TOMCAT_USAGE
+	;;
 esac    
 exit 0
